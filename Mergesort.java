a404daf324a3bda94bbf37126ec2baed11735fa3
sotosoldevillajatwit <114027980+sotosoldevillaj@users.noreply.github.com>
2024-01-19 14:38:02 -0500

    Update Mergesort.java

--- Mergesort.java
+++ Mergesort.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 
-public class Mergesort(int[],int,int) {
+public class Mergesort {
     void merge(int arr[], int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
