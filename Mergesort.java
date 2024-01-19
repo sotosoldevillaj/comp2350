ceb8738acf9066e18a5c7a56747fb180b836afea
sotosoldevillajatwit <114027980+sotosoldevillaj@users.noreply.github.com>
2024-01-19 14:34:22 -0500

    Update Mergesort.java

--- Mergesort.java
+++ Mergesort.java
@@ -35,14 +35,14 @@ public class Mergesort {
         }
     }
 
-    public void sort(int arr[], int l, int r) {
+    void sort(int arr[], int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
             sort(arr, l, m);
             sort(arr, m + 1, r);
             merge(arr, l, m, r);
         }
-    public void mergeSort(int arr[], int l, int r) {
+    void mergeSort(int arr[], int l, int r) {
         sort(arr, l, r);
     }
 }
