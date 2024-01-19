e017d31d0074f2f8a8dbcd5b23a6d95f1e15e7bb
sotosoldevillajatwit <114027980+sotosoldevillaj@users.noreply.github.com>
2024-01-19 16:31:03 -0500

    Update Mergesort.java

--- Mergesort.java
+++ Mergesort.java
@@ -55,17 +55,4 @@ class MergeSort {
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-
-    public static void main(String args[]) {
-        int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-        System.out.println("Given array is");
-        printArray(arr);
-
-        MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length - 1);
-
-        System.out.println("\nSorted array is");
-        printArray(arr);
-    }
 }
